67b6223f3d19d66670588c9ead96c9a8665a333d
WhiteSprite <1456014916@qq.com>
2026-09-21 14:50:13 +0800

    chore(deps): update Gradle, Kotlin, Micronaut, and related dependencies
    - Upgraded Gradle to 9.7.1
    - Updated Kotlin plugins to 2.4.20
    - Bumped Micronaut dependencies to latest versions (e.g., Micronaut to 5.2.2, Micronaut Platform to 5.1.5)
    - Adjusted repository configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-    kotlin("jvm") version "2.4.10" apply false
-    id("com.google.devtools.ksp") version "2.3.11" apply false
-    kotlin("plugin.allopen") version "2.4.10" apply false
+    kotlin("jvm") version "2.4.20" apply false
+    id("com.google.devtools.ksp") version "2.3.12" apply false
+    kotlin("plugin.allopen") version "2.4.20" apply false
     id("io.micronaut.application") version "5.0.2" apply false
-    id("org.graalvm.buildtools.native") version "1.1.9" apply false
+    id("org.graalvm.buildtools.native") version "1.1.13" apply false
 }
 
 tasks.wrapper {
-    gradleVersion = "9.7.0"
+    gradleVersion = "9.7.1"
     distributionType = Wrapper.DistributionType.ALL
     distributionUrl = "https://mirrors.cloud.tencent.com/gradle/gradle-${gradleVersion}-all.zip"
 }
 
 val commonRepositories: RepositoryHandler.() -> Unit = {
 //    maven("https://mirrors.huaweicloud.com/repository/maven/")
-//    maven("https://maven.aliyun.com/repository/public/")
+    maven("https://maven.aliyun.com/repository/public/")
     maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
     mavenCentral()
     google()
